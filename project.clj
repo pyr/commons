a2e8eaf826df3717d910993d9deae4981afe3984
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-09-12 13:04:10 +0200

    Version 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/commons "0.3.2-SNAPSHOT"
+(defproject spootnik/commons "0.3.2"
   :description "No-dependency convenience utilities"
   :url "https://github.com/pyr/commons"
   :license {:name "MIT/ISC License"
